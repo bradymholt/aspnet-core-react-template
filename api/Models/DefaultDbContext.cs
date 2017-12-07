587c98161da891840c5079c28f73b3223a847fcb
Alexandru Nastase <alexandru.nastase@outlook.com>
2017-12-07 06:28:35 +0200

    Add more comments to document database changes from PascalCase to snake_case.

--- api/Models/DefaultDbContext.cs
+++ api/Models/DefaultDbContext.cs
@@ -19,33 +19,37 @@ namespace aspnetCoreReactTemplate.Models
     {
       base.OnModelCreating(modelBuilder);
 
-      // Replace table names
       foreach(var entity in modelBuilder.Model.GetEntityTypes())
       {
-
+        // Remove 'AspNet' prefix and convert table name from PascalCase to snake_case. E.g. AspNetRoleClaims -> role_claims
         entity.Relational().TableName = entity.Relational().TableName.Replace("AspNet", "").ToSnakeCase();
 
-        // Replace column names
+        // Convert column names from PascalCase to snake_case.
         foreach(var property in entity.GetProperties())
         {
           property.Relational().ColumnName = property.Name.ToSnakeCase();
         }
-
+        
+        // Convert primary key names from PascalCase to snake_case. E.g. PK_users -> pk_users
         foreach(var key in entity.GetKeys())
         {
           key.Relational().Name = key.Relational().Name.ToSnakeCase();
         }
 
+        // Convert foreign key names from PascalCase to snake_case.
         foreach(var key in entity.GetForeignKeys())
         {
           key.Relational().Name = key.Relational().Name.ToSnakeCase();
         }
-
+        
+        // Convert index names from PascalCase to snake_case.
         foreach(var index in entity.GetIndexes())
         {
           index.Relational().Name = index.Relational().Name.ToSnakeCase();
         }
       }
+      
     }
+
   }
 }

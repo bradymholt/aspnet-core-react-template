9c6f0647957d0370d12b70be6a27fb5972618243
Brady Holt <brady.holt@gmail.com>
2020-01-20 07:59:07 -0600

    Fix issues with running tests

--- client-react.test/tests.tsx
+++ client-react.test/tests.tsx
@@ -2,7 +2,7 @@ import { JSDOM } from "jsdom";
 import * as localStorage from "./polyfill/localStorage.js";
 
 before(function() {
-    const dom = new JSDOM("<!doctype html><html><body></body></html>");
+    const dom = new JSDOM("<!doctype html><html><body></body></html>", { url: "http://localhost" });
 
     (global as any).window = dom.window;
     (global as any).document = dom.window.document;
